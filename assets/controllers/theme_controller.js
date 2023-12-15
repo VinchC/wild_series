beeb740e868a08748d14ad8e88869848ba377ccc
Vincent C <vinch.cm@gmail.com>
2023-12-15 16:48:21 +0100

    quest 27 done dark mode implemented and kept if page reloaded

--- assets/controllers/theme_controller.js
+++ assets/controllers/theme_controller.js
@@ -1,9 +1,21 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ["body"];
+    static targets = ["footer", "body"];
+    darkMode = false;
+
+    connect() {
+        this.darkMode = JSON.parse(localStorage.getItem('darkMode')) ?? false;
+        this.updateTheme();
+    }
 
     toggleDarkMode(event) {
-        this.bodyTarget.setAttribute('data-bs-theme', 'dark');
+        this.darkMode = !this.darkMode;
+        localStorage.setItem('darkMode', this.darkMode);
+        this.updateTheme();
+    }
+
+    updateTheme() {
+        this.bodyTarget.setAttribute('data-bs-theme', this.darkMode ? 'dark' : ''); 
     }
 }
