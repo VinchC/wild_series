64b6e891911b41f26f2964c08e9e8d5c43769723
Vincent C <vinch.cm@gmail.com>
2023-12-16 16:20:19 +0100

    config done and swup implemented and tested

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Swup\SwupBundle::class => ['all' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
 ];
